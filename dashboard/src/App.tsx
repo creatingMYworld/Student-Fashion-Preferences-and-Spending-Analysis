85dea5c83db8a5e02234bb5455bf236d7c3fd51a
dhinesh054 <166216809+dhinesh054@users.noreply.github.com>
2026-03-13 22:49:39 +0530

    Update App.tsx

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Student-Fashion-Preferences-and-Spending-Analysis">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
