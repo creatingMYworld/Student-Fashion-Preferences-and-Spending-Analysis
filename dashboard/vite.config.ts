119407152f72a37e39df3ed1109fe4c71cf23a31
dhinesh054 <166216809+dhinesh054@users.noreply.github.com>
2026-03-13 22:19:13 +0530

    Update vite.config.ts

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Student-Fashion-Preferences-and-Spending-Analysis/",  // 👈 add this
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    hmr: { overlay: false },
   },
   plugins: [react()].filter(Boolean),
   resolve: {
