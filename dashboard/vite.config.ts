24988223b7bd22a9373e5bcbf219f6327f75d300
dhinesh054 <166216809+dhinesh054@users.noreply.github.com>
2026-03-13 22:53:44 +0530

    Update vite.config.ts

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/Student-Fashion-Preferences-and-Spending-Analysis/",  // 👈 add this
+  base: "/Student-Fashion-Preferences-and-Spending-Analysis/",
   server: {
     host: "::",
     port: 8080,
